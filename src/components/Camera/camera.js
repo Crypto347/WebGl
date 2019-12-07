ec310e64cc76f675618c4458a2eddc77cca9041e
Humay <crypto347@gmail.com>
2019-12-07 12:08:07 +0400

    camera view webGl added

--- src/components/Camera/camera.js
+++ src/components/Camera/camera.js
@@ -87,7 +87,10 @@ export class Camera extends Component {
             rotation: [40, 25, 325],
             scale: [1, 1, 1],
             fudgeFactor: 0,
-            fieldOfView: 92
+            fieldOfView: 92,
+            cameraAngle: 0,
+            cameraRadius: 200,
+            numberOfH: 5
         };
     }
 
@@ -476,16 +479,22 @@ export class Camera extends Component {
         let zNear = 1;
         let zFar = 2000;
 
-        let matrix = this.perspective(this.state.fieldOfView, aspect, zNear, zFar);
-        // matrix = this.multiplyMatrices(matrix, this.orthographic(0, aspect, gl.canvas.clientWidth, gl.canvas.clientHeight, 0, 400, -400));
-        matrix = this.translate(matrix, this.state.rangeX, this.state.rangeY, this.state.rangeZ);
-        matrix = this.rotateX(matrix, this.state.deg[0]);
-        matrix = this.rotateY(matrix, this.state.deg[1]);
-        matrix = this.rotateZ(matrix, this.state.deg[2]);
-        matrix = this.scale(matrix, this.state.scale[0], this.state.scale[1], this.state.scale[2]);
+        let projectionMatrix = this.perspective(this.state.fieldOfView, aspect, zNear, zFar);
+        // matrix = this.translate(matrix, this.state.rangeX, this.state.rangeY, this.state.rangeZ);
+        // matrix = this.rotateX(matrix, this.state.deg[0]);
+        // matrix = this.rotateY(matrix, this.state.deg[1]);
+        // matrix = this.rotateZ(matrix, this.state.deg[2]);
+        // matrix = this.scale(matrix, this.state.scale[0], this.state.scale[1], this.state.scale[2]);
+
+        let cameraMatrix = this.rotationMatrixY(this.state.cameraAngle);
+        cameraMatrix = this.translate(cameraMatrix, 0, 0, this.state.cameraRadius);
+
+        let viewMatrix = this.inverse(cameraMatrix);
+
+        let viewProjectionMatrix = this.multiplyMatrices(projectionMatrix, viewMatrix);
 
         // gl.uniform2f(programInfo.uniformLocations.resolution, gl.canvas.width, gl.canvas.height);
-        gl.uniformMatrix4fv(programInfo.uniformLocations.matrix, false, matrix);
+        // gl.uniformMatrix4fv(programInfo.uniformLocations.matrix, false, matrix);
         // gl.uniform1f(programInfo.uniformLocations.fudgeFactor, this.state.fudgeFactor);
 
         {
@@ -524,26 +533,27 @@ export class Camera extends Component {
                 programInfo.attribLocations.color);
         }
 
-        // for (let i = 0; i < 5; ++i) {
-        //     // Multiply the matrices.
-        //     matrix = this.translate(matrix, this.state.rangeX, this.state.rangeY);
-        //     matrix = this.rotate(matrix, this.state.deg);
-        //     matrix = this.scale(matrix, this.state.scale[0], this.state.scale[1]);
-        //     matrix = this.multiplyMatrices(matrix, moveOriginMatrix)
-         
-        //     // Set the matrix.
-        //     gl.uniformMatrix3fv(programInfo.uniformLocations.matrix, false, matrix);
-         
-        //     // Draw the geometry.
-        //     gl.drawArrays(gl.TRIANGLES, 0, 18);
-        // }
+        for (let i = 0; i < this.state.numberOfH; ++i) {
+            let angle = i * Math.PI * 2 / this.state.numberOfH;
+            let x = Math.cos(angle) * this.state.cameraRadius;
+            let y = Math.sin(angle) * this.state.cameraRadius;
 
-        {
-            const vertexCount = 18 * 9;
-            const type = gl.TRIANGLES;
-            const offset = 0;
-            gl.drawArrays(type, offset, vertexCount);
+            // Multiply the matrices.
+            let matrix = this.translate(viewProjectionMatrix, x, 0, y);
+         
+            // Set the matrix.
+            gl.uniformMatrix4fv(programInfo.uniformLocations.matrix, false, matrix);
+         
+            // Draw the geometry.
+            gl.drawArrays(gl.TRIANGLES, 0, 18 * 9);
         }
+
+        // {
+        //     const vertexCount = 18 * 9;
+        //     const type = gl.TRIANGLES;
+        //     const offset = 0;
+        //     gl.drawArrays(type, offset, vertexCount);
+        // }
        
     }
 
@@ -655,21 +665,106 @@ export class Camera extends Component {
         ]
     }
 
-    // projectionMatrix = (width, height, depth) => {
-    //     // Note: This matrix flips the Y axis so 0 is at the top.
-    //     return [
-    //         2 / width, 0, 0, 0,
-    //         0, -2 / height, 0, 0,
-    //         0, 0, 2 / depth, 0,
-    //        -1, 1, 0, 1,
-    //     ]
-    // }
+    projectionMatrix = (width, height, depth) => {
+        // Note: This matrix flips the Y axis so 0 is at the top.
+        return [
+            2 / width, 0, 0, 0,
+            0, -2 / height, 0, 0,
+            0, 0, 2 / depth, 0,
+           -1, 1, 0, 1,
+        ]
+    }
 
     identity = () => {
         return [
             1, 0, 0,
             0, 1, 0,
             0, 0, 1]
+    }
+
+    inverse = (m) => {
+        let m00 = m[0 * 4 + 0];
+        let m01 = m[0 * 4 + 1];
+        let m02 = m[0 * 4 + 2];
+        let m03 = m[0 * 4 + 3];
+        let m10 = m[1 * 4 + 0];
+        let m11 = m[1 * 4 + 1];
+        let m12 = m[1 * 4 + 2];
+        let m13 = m[1 * 4 + 3];
+        let m20 = m[2 * 4 + 0];
+        let m21 = m[2 * 4 + 1];
+        let m22 = m[2 * 4 + 2];
+        let m23 = m[2 * 4 + 3];
+        let m30 = m[3 * 4 + 0];
+        let m31 = m[3 * 4 + 1];
+        let m32 = m[3 * 4 + 2];
+        let m33 = m[3 * 4 + 3];
+        let tmp_0  = m22 * m33;
+        let tmp_1  = m32 * m23;
+        let tmp_2  = m12 * m33;
+        let tmp_3  = m32 * m13;
+        let tmp_4  = m12 * m23;
+        let tmp_5  = m22 * m13;
+        let tmp_6  = m02 * m33;
+        let tmp_7  = m32 * m03;
+        let tmp_8  = m02 * m23;
+        let tmp_9  = m22 * m03;
+        let tmp_10 = m02 * m13;
+        let tmp_11 = m12 * m03;
+        let tmp_12 = m20 * m31;
+        let tmp_13 = m30 * m21;
+        let tmp_14 = m10 * m31;
+        let tmp_15 = m30 * m11;
+        let tmp_16 = m10 * m21;
+        let tmp_17 = m20 * m11;
+        let tmp_18 = m00 * m31;
+        let tmp_19 = m30 * m01;
+        let tmp_20 = m00 * m21;
+        let tmp_21 = m20 * m01;
+        let tmp_22 = m00 * m11;
+        let tmp_23 = m10 * m01;
+    
+        let t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) -
+            (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
+        let t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) -
+            (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
+        let t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) -
+            (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
+        let t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) -
+            (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+    
+        let d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+    
+        return [
+          d * t0,
+          d * t1,
+          d * t2,
+          d * t3,
+          d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) -
+                (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
+          d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) -
+                (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
+          d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) -
+                (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
+          d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) -
+                (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
+          d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) -
+                (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
+          d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) -
+                (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
+          d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) -
+                (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
+          d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) -
+                (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
+          d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) -
+                (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
+          d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) -
+                (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
+          d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) -
+                (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
+          d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) -
+                (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02))
+        ];
     }
 
     translationMatrix = (tx, ty, tz) => {
@@ -826,7 +921,7 @@ export class Camera extends Component {
     }
 
     /**
-    * fudgeFactor
+    * Fudge factor
     */ 
 
     handleOnChangeFudgeFactor = (e) => {
@@ -838,7 +933,7 @@ export class Camera extends Component {
     }
 
     /**
-    * field of view
+    * Field of view
     */ 
 
     handleOnChangeFieldOfView = (e) => {
@@ -848,6 +943,20 @@ export class Camera extends Component {
         });
         this.updateCanvas();
     }
+
+    /**
+    * Camera angle
+    */ 
+
+    handleOnChangeCameraAngle = (e) => {
+        let updatedCameraAngle = +e.target.value;
+        this.setState({
+            cameraAngle: updatedCameraAngle
+        });
+        this.updateCanvas();
+    }
+
+    
     
     /**
     * Markup
@@ -880,6 +989,8 @@ export class Camera extends Component {
                     <input type="range" value={this.state.sfudgeFactor} min="0" max="2" step="0.001" onChange={() => this.handleOnChangeFudgeFactor(event)}/> */}
                     <div>fieldOfView({this.state.fieldOfView})</div>
                     <input type="range" value={this.state.fieldOfView} min="0" max="179" step="1" onChange={() => this.handleOnChangeFieldOfView(event)}/>
+                    <div>cameraAngle({this.state.cameraAngle})</div>
+                    <input type="range" value={this.state.cameraAngle} min="-360" max="360" step="1" onChange={() => this.handleOnChangeCameraAngle(event)}/>
                 </div>
             </div> 
         );
